8bf9961fc041c4d0c9209180a30376e13eb9ffe0
prince-7 <amanvijaykumarsingh7792@gmail.com>
2022-01-24 02:47:12 +0530

    Updates

---
+++ 252f366e-6fec2b862e4c5c85ac03.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkamansingh=self.webpackChunkamansingh||[]).push([[228],{2775:function(t,n,a){a.d(n,{hdK:function(){return e}});var h=a(1046);function e(t){return(0,h.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-6fec2b862e4c5c85ac03.js.map
